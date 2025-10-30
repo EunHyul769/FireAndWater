8176d9342959b0e3d2f677bfa69d129e47895362
KimJiHun <gowvw777@gmail.com>
2025-10-30 20:53:29 +0900

    feat : 타이틀 씬 최소기능 구현&스테이지선택씬bg삽입

---
+++ Assets/Script/TitleUIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleUIManager : MonoBehaviour
+{
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene("StageChoiceScene"); 
+    }
+
+    public void OnClickCredit()
+    {
+        SceneManager.LoadScene("CreditScene");
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
